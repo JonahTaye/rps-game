6b16bf6f56b96b90b14e9cc05321ca68a966446d
jonah <jonah.softwaredev@gmail.com>
2025-01-29 19:40:19 +0300

    perf: change check for playRound() func

--- rps-game.js
+++ rps-game.js
@@ -32,30 +32,16 @@ let computerChoice = getComputerChoice();
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
         console.log("It's a tie")
-    } else if (humanChoice == "rock") {
-        if (computerChoice == "scissors") {
-            console.log("You win!! Rock beats Scissors")
-            humanScore += 1
-        } else if (computerChoice == "paper") {
-            console.log("You lost!! Paper beats Rock")
-            computerScore += 1
-        }
-    } else if (humanChoice == "paper") {
-        if (computerChoice == "scissors") {
-            console.log("You lose!! Scissors beats Paper")
-            computerScore += 1
-        } else if (computerChoice == "rock") {
-            console.log("You win!! Rock beats Scissors")
-            humanScore += 1
-        }
-    } else if (humanChoice == "scissors") {
-        if (computerChoice == "rock") {
-            console.log("You lose!! Scissors beats Paper")
-            computerScore += 1
-        } else if (computerChoice == "paper") {
-            console.log("You win!! Paper beats Scissors")
-            humanScore += 1
-        }
+    } else if(
+        humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper"
+    ) {
+        humanScore += 1;
+        console.log(`You win!! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`)
+    } else {
+        computerScore += 1;
+        console.log(`You lose!! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`)
     }
 }
 
