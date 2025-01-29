271505804fcb05a75e0326b77be55264c8637aef
jonah <jonah.softwaredev@gmail.com>
2025-01-29 16:45:13 +0300

    feat: add getComputerChoice() func to return computer's move

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+function getComputerChoice() {
+    let move = ""
+    let random = Math.ceil(Math.random() * 3);
+
+    switch (random) {
+        case 1:
+            move = "rock";
+            break
+        case 2:
+            move = "paper";
+            break
+        default:
+            move = "scissors";     
+    }
+
+    return move
+}
+
+console.log(getComputerChoice())
