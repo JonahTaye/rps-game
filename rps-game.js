176ad27160e49afdc07f24923d8840d41e0002f4
jonah <jonah.softwaredev@gmail.com>
2025-01-29 19:49:58 +0300

    feat: add score counter when moves are tied

--- rps-game.js
+++ rps-game.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let computerMove = ""
+    let computerMove = "";
     let random = Math.ceil(Math.random() * 3);
 
     switch (random) {
@@ -32,6 +32,8 @@ let computerChoice = getComputerChoice();
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
         console.log("It's a tie")
+        humanScore += 1;
+        computerScore += 1;
     } else if(
         humanChoice == "Rock" && computerChoice == "Scissors" ||
         humanChoice == "Paper" && computerChoice == "Rock" ||
