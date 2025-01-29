b133c78cf5c1a7ff4c56c54b95d8e7b388b1e26e
jonah <jonah.softwaredev@gmail.com>
2025-01-29 20:18:23 +0300

    feat: add playGame() func
    the playGame() function declares playRound() function and score
    varaibles in the function block calls playRound() 5 times

--- rps-game.js
+++ rps-game.js
@@ -25,28 +25,31 @@ function getHumanChoice() {
     return humanMove.charAt(0).toUpperCase() + humanMove.slice(1)
 }
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log("It's a tie")
+            humanScore += 1;
+            computerScore += 1;
+        } else if(
+            humanChoice == "Rock" && computerChoice == "Scissors" ||
+            humanChoice == "Paper" && computerChoice == "Rock" ||
+            humanChoice == "Scissors" && computerChoice == "Paper"
+        ) {
+            humanScore += 1;
+            console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
+        } else {
+            computerScore += 1;
+            console.log(`You lose!! ${computerChoice} beats ${humanChoice}`);
+        }
+    }
 
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log("It's a tie")
-        humanScore += 1;
-        computerScore += 1;
-    } else if(
-        humanChoice == "Rock" && computerChoice == "Scissors" ||
-        humanChoice == "Paper" && computerChoice == "Rock" ||
-        humanChoice == "Scissors" && computerChoice == "Paper"
-    ) {
-        humanScore += 1;
-        console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
-    } else {
-        computerScore += 1;
-        console.log(`You lose!! ${computerChoice} beats ${humanChoice}`);
+        playRound(humanChoice, computerChoice);
     }
 }
 
-playRound(humanChoice, computerChoice);
-console.log("Human choice: " + humanChoice);
-console.log("Computer choice: " + computerChoice);
+playGame();
